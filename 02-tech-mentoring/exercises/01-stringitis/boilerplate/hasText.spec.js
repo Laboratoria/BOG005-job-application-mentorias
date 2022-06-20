412e83668b1749dd0a5fbfd29b848b5afd5cb633
merunga <merunga@gmail.com>
2022-06-20 12:35:10 -0500

    afeat(cs50): trial

--- 02-tech-mentoring/exercises/01-stringitis/boilerplate/hasText.spec.js
+++ 02-tech-mentoring/exercises/01-stringitis/boilerplate/hasText.spec.js
@@ -1,23 +1,19 @@
-[
-  'hasText',
-  'hasText.recursive',
-  'hasText.slice',
-].forEach((pathToSolution) => {
-  const hasText = require(`../solutions/${pathToSolution}`);
 
-  describe(pathToSolution, () => {
-    [
-      [['', ''], true],
-      [['foo', ''], true],
-      [['', 'foo'], false],
-      [['Laboratoria', 'oratoria'], true],
-      [['Equipo', 'yo'], false],
-      [['OMG', 'O'], true],
-      [['hola', 'no'], false],
-    ].forEach(([args, expected]) => {
-      it(`should return ${expected} for ${JSON.stringify(args)}`, () => {
-        expect(hasText(...args)).toBe(expected);
-      });
+const hasText = require(`../solutions/${pathToSolution}`);
+
+describe(pathToSolution, () => {
+  [
+    [['', ''], true],
+    [['foo', ''], true],
+    [['', 'foo'], false],
+    [['Laboratoria', 'oratoria'], true],
+    [['Equipo', 'yo'], false],
+    [['OMG', 'O'], true],
+    [['hola', 'no'], false],
+  ].forEach(([args, expected]) => {
+    it(`should return ${expected} for ${JSON.stringify(args)}`, () => {
+      expect(hasText(...args)).toBe(expected);
     });
   });
 });
+
